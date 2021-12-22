0cbfba85b241e875f575750f7ddd6b82a00c9e94
MohamedGEDI <gedimohammed@gmail.com>
2021-12-22 17:26:00 +0000

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- Create db if it doesnt exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
 -- Create user and password
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 
